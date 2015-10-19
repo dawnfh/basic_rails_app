09673ee5c00b329d9b2044f14a2bbf2620114f4f
dawnfh <2014fed@gmail.com>
2015-10-19 14:19:16 -0400

    updated appcontroller with helper

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,7 +5,7 @@ class UsersController < ApplicationController
 
   def create
   	@user = User.new(params[:user])
-  	if @user.save
+  	if @user.save 
   		redirect_to root_url, :notice => "Your account is active!"
   	else
   		render "new"
